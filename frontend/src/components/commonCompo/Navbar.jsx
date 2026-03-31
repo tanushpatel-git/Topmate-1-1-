1c0beccbf9bccf67aee0e0a40cb34e847bd9fda9
tanushpatel-git <tanush000patel@gmail.com>
2026-03-31 16:01:16 +0530

    change done by tanush

--- frontend/src/components/commonCompo/Navbar.jsx
+++ frontend/src/components/commonCompo/Navbar.jsx
@@ -4,9 +4,8 @@ const Navbar = () => {
   return (
     <div>
       <h1>Topmate</h1>
-<h2> hello tanush  </h2>
-<h3>heloo  tanush</h3>
-
+      <h2> hello tanush  </h2>
+      <h3>heloo  tanush</h3>
     </div>
   )
 }
