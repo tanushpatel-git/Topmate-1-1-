4f3626395c9f93fa9388e6749c2234f7c36a54b0
tanushpatel-git <tanush000patel@gmail.com>
2026-04-09 03:46:14 +0530

    make error free code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import Navbar from "../components/commonCompo/Navbar"
-import Card from "../components/HomePageComponet/Card"
-import Hero from "../components/HomePageComponet/Hero"
-import HomeNewPage from "../components/HomePageComponet/Hero2"
-import HomeCreatePage from "../components/HomePageComponet/Hero3"
-import HomeFeedbackpage from "../components/HomePageComponet/Hero4"
-import HomeFAQ from "../components/HomePageComponet/Hero5"
+import Card from "../components/HomePageComponent/Card"
+import Hero from "../components/HomePageComponent/Hero"
+import HomeNewPage from "../components/HomePageComponent/Hero2"
+import HomeCreatePage from "../components/HomePageComponent/Hero3"
+import HomeFeedbackpage from "../components/HomePageComponent/Hero4"
+import HomeFAQ from "../components/HomePageComponent/Hero5"
 import Footer from "../components/commonCompo/Footer"
 
 const Home = () => {
