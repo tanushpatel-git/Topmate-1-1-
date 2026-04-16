e93ccd6340500f560bcc206c4417a91dd95039da
tanushpatel-git <tanush000patel@gmail.com>
2026-04-17 02:45:23 +0530

    check back commit

---
+++ backend/src/utility/mongoDB.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const mongoDB = async () => {
+    try {
+        const url = process.env.MONGODB_URI;
+        if (!url) {
+            throw new Error("MONGODB_URI is not defined");
+        }
+        await mongoose.connect(url);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.log(error);
+        console.log("MongoDB connection failed");
+    }
+}
+
+module.exports = mongoDB;
