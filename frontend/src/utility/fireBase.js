00917bef5934ff9c65b9f9e689020d8a5c025099
NikeshDeveloper3999 <nikeshparte726@gmail.com>
2026-04-19 18:33:01 +0530

    add

--- frontend/src/utility/fireBase.js
+++ frontend/src/utility/fireBase.js
@@ -5,12 +5,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "topmate-244da.firebaseapp.com",
-  projectId: "topmate-244da",
-  storageBucket: "topmate-244da.firebasestorage.app",
-  messagingSenderId: "824987856658",
-  appId: "1:824987856658:web:9ee6a04c1439ff753b8dcb",
-  measurementId: "G-9YL4135KSD"
+  authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
