3a07b6d6d52fe347a50d0fbd81a5c5529a65bc68
tanushpatel-git <tanush000patel@gmail.com>
2026-05-02 01:52:42 +0530

    comlete 4 function in navbar of profile

--- frontend/src/components/CreatorDashboard/Profile.jsx
+++ frontend/src/components/CreatorDashboard/Profile.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ProfileNavbar from "../ProfileComponent/ProfileNavbar";
 import ColorSet from "../ProfileComponent/ColorSet";
+import AccountEdit from "../ProfileComponent/AccountEdit";
+import AddHighlight from "../ProfileComponent/AddHighlight";
 import { useState } from "react";
+import ArrangmentOfServicePrice from "../ProfileComponent/ArrangmentOfServicePrice";
 
 const colors = [
   "#d9534f",
@@ -14,12 +17,39 @@ const colors = [
 const Profile = () => {
 
   const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(true);
+  const [open4, setOpen4] = useState(false);
   const [selectedColor, setSelectedColor] = useState(colors[4]);
 
   return (
     <>
-      <ProfileNavbar selectedColor={selectedColor} onClose={() => setOpen(!open)} />
-      <ColorSet isOpen={open} onClose={() => setOpen(!open)} colors={colors} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+      <ProfileNavbar
+        onClose4={() => setOpen4(!open4)}
+        onClose3={() => setOpen3(!open3)}
+        onClose2={() => setOpen2(!open2)}
+        selectedColor={selectedColor}
+        onClose={() => setOpen(!open)}
+      />
+      <ColorSet
+        isOpen={open}
+        onClose={() => setOpen(!open)}
+        colors={colors}
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+      />
+      <AccountEdit
+        isOpen={open2}
+        onClose={() => setOpen2(!open2)}
+      />
+      <AddHighlight
+        isOpen={open3}
+        onClose={() => setOpen3(!open3)}
+      />
+      <ArrangmentOfServicePrice
+        isOpen={open4}
+        onClose={() => setOpen4(!open4)}
+      />
     </>
   );
 };
